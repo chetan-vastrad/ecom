ee624804096d8541bfc1992e9519af1ccf5ce1de
chetannumasoft <chetannumasoft@gmail.com>
2025-10-24 19:23:15 +0530

    file name changed

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,7 @@ app.use("/api/products", productRoutes);
 // }
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/^\/.*$/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
